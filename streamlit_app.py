80095513da2b05dffb70aa319a1a80a61363ec99
Sakthivel-N <sakthiwebdev3520@gmail.com>
2022-12-21 14:29:17 +0530

    asdas

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,10 @@ def get_results(query,s):
                 my_cur.execute(q)
             result= my_cur.fetchall()
             streamlit.dataframe(result)
-            for i in result:
-                streamlit.write(i[0])
             if(s != 'NO'):
                 inputval = streamlit.selectbox("Enter your "+s+" : ", list(i[0] for i in result))
                 returnval = re.findall(r"'(.*?)'", str(inputval), re.DOTALL)
                 return(returnval[0])
-            
             
         finally:
             my_cur.close()
