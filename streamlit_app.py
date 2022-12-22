aae39d9aaa126a3170680e0c5b4b6350e35c0e73
Sakthivel-N <sakthiwebdev3520@gmail.com>
2022-12-22 11:53:04 +0530

    werew

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from io import StringIO
 
 streamlit.title("Data Lineage");
 
-@streamlit.cache(allow_output_mutation=True)
+@streamlit.cache
 def get_results(query,s):
 
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
