c578e97d5784d4b5bde40a00ed5b00c2ca5f6e6e
Sakthivel-N <sakthiwebdev3520@gmail.com>
2022-12-22 16:28:30 +0530

    asdas

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,57 +48,55 @@ with streamlit.form("first_form2"):
     streamlit.header('Select your Table')
     # ##Table
     tableval = get_results([f"select DISTINCT(table_name) from SNOWFLAKE.INFORMATION_SCHEMA.TABLE_STORAGE_METRICS where table_catalog = '"+dbval+"' and table_schema ='"+schemaval+"';"],"Table")
-    streamlit.form_submit_button(label='APPLY')
     
-    if  streamlit.form_submit_button(label='APPLY'):
+    
+    streamlit.header('All DML Changes')
+    # ##all Dml changes
+    get_results([f"call Data_lineage.PUBLIC.sp_dl_histroy('"+dbval+"."+schemaval+"."+tableval+"',1);",f"call Data_lineage.PUBLIC.sp_dl();",f"select * from Data_lineage.PUBLIC.employee_changes order by start_time ;"],'NO')
+    streamlit.header('All Inserts')
+    # ##inser all
+    get_results([f"select * from Data_lineage.PUBLIC.employee_changes where metadata$action ='INSERT' and metadata$isupdate='false' order by start_time;"],'NO')
 
-        streamlit.header('All DML Changes')
-        # ##all Dml changes
-        get_results([f"call Data_lineage.PUBLIC.sp_dl_histroy('"+dbval+"."+schemaval+"."+tableval+"',1);",f"call Data_lineage.PUBLIC.sp_dl();",f"select * from Data_lineage.PUBLIC.employee_changes order by start_time ;"],'NO')
-        streamlit.header('All Inserts')
-        # ##inser all
-        get_results([f"select * from Data_lineage.PUBLIC.employee_changes where metadata$action ='INSERT' and metadata$isupdate='false' order by start_time;"],'NO')
+    streamlit.header('All Updates')
+    ##update all
+    get_results([f"select * from Data_lineage.PUBLIC.employee_changes where (metadata$action ='INSERT' or metadata$action ='DELETE' )and metadata$isupdate='true' order by start_time;"],'NO')
 
-        streamlit.header('All Updates')
-        ##update all
-        get_results([f"select * from Data_lineage.PUBLIC.employee_changes where (metadata$action ='INSERT' or metadata$action ='DELETE' )and metadata$isupdate='true' order by start_time;"],'NO')
-
-        streamlit.header('All Deletes')
-        ##delete all
-        get_results([f"select * from Data_lineage.PUBLIC.employee_changes where metadata$action ='DELETE' and metadata$isupdate='false' order by  start_time;"],'NO')
-
+    streamlit.header('All Deletes')
+    ##delete all
+    get_results([f"select * from Data_lineage.PUBLIC.employee_changes where metadata$action ='DELETE' and metadata$isupdate='false' order by  start_time;"],'NO')
 
 
-        streamlit.header('Enter your Column')
-        ##column
-        COLUMNVAL = get_results([f"select column_name from "+dbval+".information_schema.columns where table_catalog = '"+dbval+"' and table_schema = '"+schemaval+"' and table_name = '" + tableval+"';"],"Column")
+
+    streamlit.header('Enter your Column')
+    ##column
+    COLUMNVAL = get_results([f"select column_name from "+dbval+".information_schema.columns where table_catalog = '"+dbval+"' and table_schema = '"+schemaval+"' and table_name = '" + tableval+"';"],"Column")
 
 
-        ##columnval
-        SEARCHVAL  = streamlit.text_input("Enter "+COLUMNVAL+" value : " )
-        
+    ##columnval
+    SEARCHVAL  = streamlit.text_input("Enter "+COLUMNVAL+" value : " )
+    
 
-        ## FOR COLUMN 
-        #
-        # all dml
-        if SEARCHVAL:
-            streamlit.header('All DML on column')
-            get_results([f" select * from Data_lineage.PUBLIC.employee_changes  WHERE "+COLUMNVAL+" = "+ SEARCHVAL+" order by start_time;"],'NO')
+    ## FOR COLUMN 
+    #
+    # all dml
+    if SEARCHVAL:
+        streamlit.header('All DML on column')
+        get_results([f" select * from Data_lineage.PUBLIC.employee_changes  WHERE "+COLUMNVAL+" = "+ SEARCHVAL+" order by start_time;"],'NO')
 
-            streamlit.header('All Insert on column')
-            #insert only
-            get_results([f"select * from Data_lineage.PUBLIC.employee_changes where metadata$action ='INSERT' and metadata$isupdate='false' AND "+COLUMNVAL+" = "+ SEARCHVAL+" order by start_time;"],'NO')
+        streamlit.header('All Insert on column')
+        #insert only
+        get_results([f"select * from Data_lineage.PUBLIC.employee_changes where metadata$action ='INSERT' and metadata$isupdate='false' AND "+COLUMNVAL+" = "+ SEARCHVAL+" order by start_time;"],'NO')
 
-            streamlit.header('All Updates on column')
-            #update only
-            get_results([f"select * from Data_lineage.PUBLIC.employee_changes where (metadata$action ='INSERT' or metadata$action ='DELETE' )and metadata$isupdate='true' AND "+COLUMNVAL+" = "+ SEARCHVAL+" order by start_time;"],'NO')
+        streamlit.header('All Updates on column')
+        #update only
+        get_results([f"select * from Data_lineage.PUBLIC.employee_changes where (metadata$action ='INSERT' or metadata$action ='DELETE' )and metadata$isupdate='true' AND "+COLUMNVAL+" = "+ SEARCHVAL+" order by start_time;"],'NO')
 
-            streamlit.header('All Deletes on column')
-            #delete only
-            get_results([f"select * from Data_lineage.PUBLIC.employee_changes where metadata$action ='DELETE' and metadata$isupdate='false'  AND "+COLUMNVAL+" = "+ SEARCHVAL+" order by  start_time;"],'NO')
+        streamlit.header('All Deletes on column')
+        #delete only
+        get_results([f"select * from Data_lineage.PUBLIC.employee_changes where metadata$action ='DELETE' and metadata$isupdate='false'  AND "+COLUMNVAL+" = "+ SEARCHVAL+" order by  start_time;"],'NO')
 
 
-        streamlit.form_submit_button(label='APPLY')
+    streamlit.form_submit_button(label='APPLY')
 
 # def get_db():
 #     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
